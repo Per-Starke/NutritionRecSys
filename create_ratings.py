b592f28442addf8f1c53d3fc84dcc4f2f33533f5
Per Starke <per@starke-team.de>
2023-01-20 15:08:50 +0100

    create random ratings file

---
+++ create_ratings.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from random import randint
+
+recipe_info = pd.read_csv("recipe_database.csv")
+ids = recipe_info["ID"]
+
+with open("ratings.csv", "w+") as file:
+    for i in range(1, 100):
+        j = 0
+        while j < i:
+            str_to_write = str(i) + ", " + str(ids[randint(0, 49)]) + ", " + str(randint(1, 5)) + "\n"
+            file.write(str_to_write)
+            j += 1
+
