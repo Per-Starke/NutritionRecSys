14748f6ca77aaa7ee179d5204090659b68607f24
Per Starke <per@starke-team.de>
2023-03-14 22:28:21 +0100

    basic flask experiments

---
+++ App/try_flask.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import pandas as pd
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+"""
+@app.route("/home")
+def home():
+    parent_dir = os.path.dirname(os.getcwd())
+    recipe_database_path_and_filename = parent_dir + "/NutritionRecSys/Data/recipe_database.csv"
+    recipe_info = pd.read_csv(recipe_database_path_and_filename, index_col=False)
+
+    return str(recipe_info.head())
+"""
