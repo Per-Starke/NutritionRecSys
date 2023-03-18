8cc96762a753bf7d0c328b296b956e1047e6bebb
Per Starke <per@starke-team.de>
2023-03-18 13:57:47 +0100

    provide and link stylesheet

--- App/try_flask.py
+++ App/try_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import pandas as pd
 import os
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("home.html", name="Susi")
 
 
 if __name__ == "__main__":
