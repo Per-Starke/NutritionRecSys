a8349efcdc0b61039b225570b635c24aecd6f53c
Per Starke <per@starke-team.de>
2023-03-21 14:55:19 +0100

    show recommendations on webpage

--- App/try_flask.py
+++ App/try_flask.py
@@ -1,26 +1,33 @@
 from flask import Flask, render_template, url_for
 import pandas as pd
 import os
+import Run.output
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def home_page():
+
     return render_template("home.html")
+
+
+@app.route("/get_rec")
+def get_rec_page():
+
+    user_id = 5
+    recipes_and_ratings = Run.output.get_calculated_ratings_for_user(user_id, Run.output.write_recommendations())
+
+    content_based = recipes_and_ratings["content-based"]
+    cb_with_titles = {}
+
+    for recipe_id, rating in content_based.items():
+        cb_with_titles[Run.output.get_recipe_title_by_id(recipe_id)] = rating
+
+    return render_template("get_rec.html", recipes_and_ratings=cb_with_titles)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-"""
-@app.route("/home")
-def home():
-    parent_dir = os.path.dirname(os.getcwd())
-    recipe_database_path_and_filename = parent_dir + "/NutritionRecSys/Data/recipe_database.csv"
-    recipe_info = pd.read_csv(recipe_database_path_and_filename, index_col=False)
-
-    return str(recipe_info.head())
-"""
