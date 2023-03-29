b2674e1077bcf15b96e7216809970a3a4c2a20a2
Per Starke <per@starke-team.de>
2023-03-29 19:06:24 +0200

    start using session instead of global variables

--- App/app.py
+++ App/app.py
@@ -1,14 +1,11 @@
 import requests
 import os
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import Run.output
 import Run.recommend_for_user
 
 app = Flask(__name__)
-
-user_id = 0
-prediction_needs_updating = True
-recipe_id = None
+app.secret_key = os.urandom(12)
 
 
 @app.route("/", methods=['POST', 'GET'])
@@ -17,6 +14,8 @@ def home_page():
     Create the home-page
     """
 
+    session['prediction_needs_updating'] = True
+    session['user_id'] = 0
     return render_template("home.html")
 
 
@@ -26,42 +25,37 @@ def get_rec_page():
     Create the get-recommendations-page
     """
 
-    global user_id
-    global prediction_needs_updating
-    global recipe_id
-    # todo sessions
-
-    if prediction_needs_updating:
+    if session['prediction_needs_updating']:
         Run.output.run_recommendation_algos()
-        prediction_needs_updating = False
+        session['prediction_needs_updating'] = False
 
     if request.method == 'POST':
-        user_id = request.form['update_id']
-        if not user_id or not user_id.isdigit():
+        session['user_id'] = request.form['update_id']
+        if not session['user_id'] or not session['user_id'].isdigit():
             return render_template("error.html",
                                    error_text="this is no valid user id!", return_link="/get_rec")
         return redirect("/get_rec")
 
     # Create data-structure for displaying given ratings
-    given_ratings = Run.output.get_ratings_for_user(user_id)
+    given_ratings = Run.output.get_ratings_for_user(session['user_id'])
     given_ratings_with_titles = {}
-    for recipe_id, rating in given_ratings.items():
-        given_ratings_with_titles[Run.output.get_recipe_title_by_id(recipe_id)] = rating
+    for current_recipe_id, rating in given_ratings.items():
+        given_ratings_with_titles[Run.output.get_recipe_title_by_id(current_recipe_id)] = rating
     sorted_given_ratings_with_titles = \
         {k: v for k, v in sorted(given_ratings_with_titles.items(), key=lambda item: item[1], reverse=True)}
 
     # Create data-structure for displaying predicted ratings
     recipes_and_ratings = Run.output.get_calculated_ratings_for_user(
-        user_id, Run.output.write_recommendations(), ratings_to_get=3, userknn=False)
+        session['user_id'], Run.output.write_recommendations(), ratings_to_get=3, userknn=False)
     ids = {}
 
     try:
         content_based = recipes_and_ratings["content-based"]
         cb_with_titles = {}
         cb_ids = []
-        for recipe_id, rating in content_based.items():
-            cb_with_titles[Run.output.get_recipe_title_by_id(recipe_id)] = rating
-            cb_ids.append(recipe_id)
+        for current_recipe_id, rating in content_based.items():
+            cb_with_titles[Run.output.get_recipe_title_by_id(current_recipe_id)] = rating
+            cb_ids.append(current_recipe_id)
         recipes_and_ratings["content-based"] = cb_with_titles
         ids["content-based"] = cb_ids
     except KeyError:
@@ -71,9 +65,9 @@ def get_rec_page():
         itemknn = recipes_and_ratings["item-knn"]
         itemknn_with_titles = {}
         itemknn_ids = []
-        for recipe_id, rating in itemknn.items():
-            itemknn_with_titles[Run.output.get_recipe_title_by_id(recipe_id)] = rating
-            itemknn_ids.append(recipe_id)
+        for current_recipe_id, rating in itemknn.items():
+            itemknn_with_titles[Run.output.get_recipe_title_by_id(current_recipe_id)] = rating
+            itemknn_ids.append(current_recipe_id)
         recipes_and_ratings["item-knn"] = itemknn_with_titles
         ids["item-knn"] = itemknn_ids
     except KeyError:
@@ -83,59 +77,50 @@ def get_rec_page():
         userknn = recipes_and_ratings["user-knn"]
         userknn_with_titles = {}
         userknn_ids = []
-        for recipe_id, rating in userknn.items():
-            userknn_with_titles[Run.output.get_recipe_title_by_id(recipe_id)] = rating
-            userknn_ids.append(recipe_id)
+        for current_recipe_id, rating in userknn.items():
+            userknn_with_titles[Run.output.get_recipe_title_by_id(current_recipe_id)] = rating
+            userknn_ids.append(current_recipe_id)
         recipes_and_ratings["user-knn"] = userknn_with_titles
-        ids["user-knn"] = itemknn_ids
+        ids["user-knn"] = userknn_ids
     except KeyError:
         pass
 
-    return render_template("get_rec.html", user_id=user_id, given_ratings=sorted_given_ratings_with_titles,
+    return render_template("get_rec.html", user_id=session['user_id'], given_ratings=sorted_given_ratings_with_titles,
                            recipes_and_ratings=recipes_and_ratings, ids=ids)
 
 
 @app.route("/rate", methods=['POST', 'GET'])
 def rate_page():
     """
-    Create the get-recommendations-page
+    Create the rate-recipes-page
     """
 
-    global user_id
-    global prediction_needs_updating
-    global recipe_id
-    old_recipe_id = recipe_id
+    session['recipe_id'] = Run.recommend_for_user.get_recipe_to_rate(session['user_id'])
 
-    recipe_id = Run.recommend_for_user.get_recipe_to_rate(user_id)
-
-    if recipe_id:
-        recipe_title = Run.output.get_recipe_title_by_id(recipe_id)
+    if session['recipe_id']:
+        session['recipe_title'] = Run.output.get_recipe_title_by_id(session['recipe_id'])
     else:
-        recipe_title = "No unrated recipe found!"
+        session['recipe_title'] = "No unrated recipe found!"
 
     if request.method == 'POST':
         try:
-            user_id = request.form['update_id']
-            if not user_id or not user_id.isdigit():
+            session['user_id'] = request.form['update_id']
+            if not session['user_id'] or not session['user_id'].isdigit():
                 return render_template("error.html",
                                        error_text="this is no valid user id!", return_link="/rate")
-            recipe_id = Run.recommend_for_user.get_recipe_to_rate(user_id)
-            if recipe_id:
-                recipe_title = Run.output.get_recipe_title_by_id(recipe_id)
-            else:
-                recipe_title = "No unrated recipe found!"
             return redirect("/rate")
         except KeyError:
-            rating = request.form['get_rating']
-            if Run.recommend_for_user.check_input(rating):
-                Run.recommend_for_user.write_rating_to_file(user_id, old_recipe_id, rating)
-                prediction_needs_updating = True
+            session['rating'] = request.form['get_rating']
+            if Run.recommend_for_user.check_input(session['rating']):
+                Run.recommend_for_user.write_rating_to_file(session['user_id'], session['recipe_id'], session['rating'])
+                session['prediction_needs_updating'] = True
             else:
                 return render_template("error.html",
                                        error_text="this is no valid rating!", return_link="/rate")
             return redirect("/rate")
 
-    return render_template("rate.html", user_id=user_id, recipe_title=recipe_title, recipe_id=recipe_id)
+    return render_template("rate.html", user_id=session['user_id'], recipe_title=session['recipe_title'],
+                           recipe_id=session['recipe_id'])
 
 
 @app.route('/recipe')
@@ -149,7 +134,7 @@ def get_recipe():
     url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/"
     headers = {
         'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-        'x-rapidapi-key': rapid_api_key }
+        'x-rapidapi-key': rapid_api_key}
     single_recipe_id = request.args["id"]
     recipe_info_endpoint = "recipes/{0}/information".format(single_recipe_id)
     recipe_info = requests.request("GET", url + recipe_info_endpoint, headers=headers,
