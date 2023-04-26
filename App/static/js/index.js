7209a4e21936bc4684c5f3281b40084a6c1985c1
Per Starke <per@starke-team.de>
2023-04-26 15:16:35 +0200

    sort css indentations, external instead of inline javascript

--- App/static/js/index.js
+++ App/static/js/index.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to the navigation when the user clicks on the icon */
 function extendNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "main-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "main-nav";
-  }
-}
+    var x = document.getElementById("myTopnav");
+        if (x.className === "nav main-nav") {
+            x.className += " responsive";
+        } else {
+            x.className = "nav main-nav";
+        }
+    }
