4b47046db8f93e50bdecb132bb008075e8cff492
Per Starke <per@starke-team.de>
2023-04-28 16:13:27 +0200

    show 10 rated recipe, extend on click
    Close #88

--- App/static/js/index.js
+++ App/static/js/index.js
@@ -7,3 +7,17 @@ function extendNav() {
             x.className = "nav main-nav";
         }
     }
+
+function showMoreRecipes() {
+    var button = document.getElementById("show-all-recipes");
+    var recipes = document.getElementById("ratedRecipes").children;
+    for(var i = 0; i < recipes.length; i++) {
+        if (recipes[i].className === "body content more"){
+            recipes[i].className = "body content more-shown";
+            button.innerHTML = "show less";
+        } else if (recipes[i].className === "body content more-shown"){
+            recipes[i].className = "body content more";
+            button.innerHTML = "show all";
+        }
+    }
+}
